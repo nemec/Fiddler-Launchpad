f911b789f7a7cabaec25729b7a17e87f253afef0
Dan Nemec <dan@nem.ec>
2014-07-17 02:17:47 -0500

    Initial commit of Fiddler extension
    Adds new tab and lets you drop sessions into treeview.

---
+++ Launchpad/Launchpad.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Fiddler;
+
+[assembly: RequiredVersion("4.0.0.0")]
+
+
+namespace Launchpad
+{
+
+    public class Launchpad : IFiddlerExtension
+    {
+        private readonly TabPage _tab;
+        private readonly LaunchpadView _view;
+
+        private static readonly object TreeviewNodeSentinel = new object();
+
+        public Launchpad()
+        {
+            _tab = new TabPage("Launchpad");
+            
+            _view = new LaunchpadView
+            {
+                Dock = DockStyle.Fill
+            };
+            
+            _tab.Controls.Add(_view);
+            var treeview = _view.BookmarkTreeview;
+            treeview.DragDrop += OnItemDropped;
+            treeview.DragOver += OnDragOver;
+            treeview.ItemDrag += OnItemDrag;
+
+            RestoreBookmarks();
+        }
+
+        private void RestoreBookmarks()
+        {
+            var tree = _view.BookmarkTreeview;
+
+            var node = tree.Nodes.Add("GMD - ITG");
+            node.Tag = TreeviewNodeSentinel;
+            node.Expand();
+        }
+
+        public void OnBeforeUnload()
+        {
+        }
+
+        public void OnLoad()
+        {
+            FiddlerApplication.UI.tabsViews.TabPages.Add(_tab);
+            FiddlerApplication.UI.tabsViews.SelectedIndexChanged += OnTabSelected;
+
+        }
+
+        private void OnTabSelected(object sender, EventArgs args)
+        {
+            if (FiddlerApplication.isClosing || FiddlerApplication.UI.tabsViews.SelectedTab != _tab)
+            {
+                return;
+            }
+            EnsureReady();
+        }
+
+        private void EnsureReady()
+        {
+            FiddlerApplication.UI.tabsViews.SelectedIndexChanged -= OnTabSelected;
+
+            // Setup Fiddler things
+        }
+
+        // Drag a treeview node (to session window)
+        private void OnItemDrag(object sender, ItemDragEventArgs e)
+        {
+            
+        }
+
+        private IEnumerable<Session> GetSelectedSessions()
+        {
+            var tree = _view.BookmarkTreeview;
+            var rootNode = tree.SelectedNode;
+
+            var session = rootNode.Tag as Session;
+            return session != null 
+                ? new[]{session}
+                : GetRecursiveSessionsForNode(rootNode);
+        }
+
+        private IEnumerable<Session> GetRecursiveSessionsForNode(TreeNode rootNode)
+        {
+            var sessions = new List<Session>();
+            foreach (TreeNode node in rootNode.Nodes)
+            {
+                var session = node.Tag as Session;
+                if (session != null)
+                {
+                    sessions.Add(session);
+                }
+                else
+                {
+                    sessions.AddRange(GetRecursiveSessionsForNode(rootNode));
+                }
+            }
+            return sessions;
+        }
+
+        // Drag session over tree node (not leaf)
+        private void OnDragOver(object sender, DragEventArgs e)
+        {
+            var tree = _view.BookmarkTreeview;
+            var dropPt = tree.PointToClient(new Point(e.X, e.Y));
+            var destinationNode = tree.GetNodeAt(dropPt);
+            if (destinationNode == null)
+            {
+                return;
+            }
+
+            var isInnerNode = ReferenceEquals(destinationNode.Tag, TreeviewNodeSentinel);
+            e.Effect = isInnerNode &&
+                       (e.Data.GetDataPresent("Fiddler.Session[]") ||
+                       e.Data.GetDataPresent(DataFormats.FileDrop))
+                ? DragDropEffects.Copy
+                : DragDropEffects.None;
+        }
+
+        // Drop session into tree
+        private void OnItemDropped(object sender, DragEventArgs e)
+        {
+            var sessionArray = (Session[])e.Data.GetData("Fiddler.Session[]");
+            var tree = _view.BookmarkTreeview;
+            if (sessionArray == null || sessionArray.Length < 1 || 
+                tree == null)
+            {
+                return;
+            }
+
+            var dropPt = tree.PointToClient(new Point(e.X, e.Y));
+            var destinationNode = tree.GetNodeAt(dropPt);
+            if (destinationNode == null)
+            {
+                return;
+            }
+
+            Func<Session, bool> filterExisting = s =>
+                destinationNode.Nodes.OfType<TreeNode>().All(n =>
+                    ReferenceEquals(n.Tag, TreeviewNodeSentinel) || ((Session) n.Tag).id != s.id);
+
+            foreach (var session in sessionArray.Where(filterExisting))
+            {
+                var label = session.url;
+                destinationNode.Nodes.Add(new TreeNode(label)
+                {
+                    Tag = session
+                });
+            }
+
+        }
+    }
+}
---
+++ Launchpad/LaunchpadView.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Launchpad
+{
+    partial class LaunchpadView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BookmarkTreeview = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // BookmarkTreeview
+            // 
+            this.BookmarkTreeview.AllowDrop = true;
+            this.BookmarkTreeview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BookmarkTreeview.Location = new System.Drawing.Point(4, 4);
+            this.BookmarkTreeview.Name = "BookmarkTreeview";
+            this.BookmarkTreeview.Size = new System.Drawing.Size(791, 547);
+            this.BookmarkTreeview.TabIndex = 0;
+            // 
+            // LaunchpadView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.BookmarkTreeview);
+            this.Name = "LaunchpadView";
+            this.Size = new System.Drawing.Size(798, 554);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TreeView BookmarkTreeview;
+
+    }
+}
